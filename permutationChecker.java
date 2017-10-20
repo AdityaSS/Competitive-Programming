a40f1d2d6470d81888abf152fd4ac4d38cc0850c
Aditya Sharma <sharma.aditya0723@gmail.com>
2017-10-19 20:30:58 -0400

    Update permutationChecker.java

--- permutationChecker.java
+++ permutationChecker.java
@@ -1,3 +1,38 @@
+/*
+import java.util.Scanner;
+
+public class permutationChecker {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter 1 String.");
+		String s1 = scan.nextLine();
+		System.out.println("Enter 2 String.");
+		String s2 = scan.nextLine();
+		boolean b = permutationcheck(s1,s2);
+        if(b==false){
+        	System.out.println("Not permutation");
+        }else{
+        	System.out.println("Permutation.");
+        }
+        scan.close();
+	}
+
+	private static boolean permutationcheck(String s1, String s2) {
+		if(s1.length()!=s2.length()) return false;
+		int[] characters = new int[128];
+		for(int i = 0;i<s1.length();i++){
+			characters[i]++;
+		}
+		for(int i = 0;i<s2.length();i++){
+			characters[i]--;
+			if(characters[i]<0)
+				return false;
+		}
+		return true;
+	}
+}
+*/
 import java.util.Arrays;
 import java.util.Scanner;
 
