cf348469245d2b6c54cd7d0e3191c501f80faf97
Aditya Sharma <sharma.aditya0723@gmail.com>
2017-10-29 16:21:28 -0400

    Add files via upload

---
+++ subarraySum.java
@@ -0,0 +1,37 @@
+/* Here an unsorted array and a sum is given, we need to find the sub array such that the sum of the sub array is equal  
+ * to sum. If no such sub array exists we will return not found else we will retun starting index and ending index.
+ * Time Complexity------> O(2N)=O(n).
+ */
+public class subarraySum {
+
+	public static void main(String[] args) {
+		int[] a = {15, 2, 4, 8, 9, 5, 10, 1};
+        String s = sum(a,1);
+        System.out.println(s);
+	}
+
+	private static String sum(int[] a,int n) {
+		
+		int start = 0;
+		int sum= 0 ;
+		int end = -1;
+		while(start <a.length || end<a.length){
+			
+			if(sum < n){
+				end++;
+				sum += a[end];
+				
+			}
+			if(sum > n){
+				sum = sum-a[start];
+				start++;
+			}
+			if(sum==n){
+				return "Start:" + start+"  end : "+end;
+			}
+		}
+		return "Not Found";
+		
+	}
+
+}
